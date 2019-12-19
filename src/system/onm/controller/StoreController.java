025a2c4923ae29674ea51a1a5b0261379f5537f8
projectking <projectking@kakao.com>
2019-12-19 14:41:45 +0900

    .

---
+++ src/system/onm/controller/StoreController.java
@@ -0,0 +1,28 @@
+/**
+ * 아래에 나오는 클래스의 소속 패키지 경로를 설정하기
+ * 모든 자바 클래스 최상단에는 소속 패키지 경로가 먼저 나옴
+ */
+package system.onm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import system.onm.service.StoreService;
+
+/**
+ * StoreController 클래스
+ * 가게 관련 가상 URL 주소로 접속하면 호출되는 메소드를 소유한 컨트롤러 클래스
+ * @author Jo
+ */
+@Controller
+public class StoreController {
+	/**
+	 * 속성변수 선언
+	 */
+	@Autowired
+	private StoreService tore_service;	// StoreService 인터페이스를 구현받은 객체를 생성해서 저장
+
+	/**
+	 * 메소드 선언
+	 */
+}
---
+++ src/system/onm/dto/CustomerInfoDTO.java
@@ -0,0 +1,24 @@
+/**
+ * 아래에 나오는 클래스의 소속 패키지 경로를 설정하기
+ * 모든 자바 클래스 최상단에는 소속 패키지 경로가 먼저 나옴
+ */
+package system.onm.dto;
+
+/**
+ * CustomerInfoDTO 클래스
+ * 고객 정보를 검색할 때 사용할 DTO
+ * @author Jo
+ */
+public class CustomerInfoDTO {
+	/**
+	 * 속성변수 선언
+	 */
+
+	/**
+	 * 생성자 선언
+	 */
+
+	/**
+	 * 접근자, 설정자 선언
+	 */
+}
