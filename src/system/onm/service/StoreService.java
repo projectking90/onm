056eec3e61781a29e397066633da2cd0d8543e50
projectking <projectking@kakao.com>
2019-12-23 15:52:14 +0900

    Modify
    Modify method name

--- src/system/onm/service/StoreService.java
+++ src/system/onm/service/StoreService.java
@@ -26,11 +26,11 @@ public interface StoreService {
 	int insertStoreMenu(MenuDTO menuDTO);	// 가게 메뉴 추가
 	int updateStoreMenu(MenuDTO menuDTO);	// 가게 메뉴 수정
 	int deleteStoreMenu(MenuDTO menuDTO);	// 가게 메뉴 삭제
-	List<IngredientDTO> getMenuList(IngredientSearchDTO ingredient_searchDTO);	// 가게에 등록된 식자재를 가져옴
+	List<IngredientDTO> getIngredientList(IngredientSearchDTO ingredient_searchDTO);	// 가게에 등록된 식자재를 가져옴
 	int insertStoreIngredient(IngredientDTO ingredientDTO);	// 가게 식자재 추가
 	int updateStoreIngredient(IngredientDTO ingredientDTO);	// 가게 식자재 수정
 	int deleteStoreIngredient(IngredientDTO ingredientDTO);	// 가게 식자재 삭제
-	List<StoreKindDTO> getMenuList(StoreKindSearchDTO store_kind_searchDTO);	// 가게 업종 정보를 가져옴
-	int insertStoreMenu(StoreKindDTO store_kindDTO);	// 가게 업종 정보 추가
-	int updateStoreMenu(StoreKindDTO store_kindDTO);	// 가게 업종 정보 수정
+	List<StoreKindDTO> getStoreKindList(StoreKindSearchDTO store_kind_searchDTO);	// 가게 업종 정보를 가져옴
+	int insertStoreKind(StoreKindDTO store_kindDTO);	// 가게 업종 정보 추가
+	int updateStoreKind(StoreKindDTO store_kindDTO);	// 가게 업종 정보 수정
 }
