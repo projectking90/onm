4e37fa1b5b88809215f6202541096ebdc4526689
lg <lg@DESKTOP-45HRVL2>
2019-12-26 19:12:55 +0900

    dd

---
+++ WebContent/WEB-INF/resources/Store/ingredient_detail_form.js
@@ -0,0 +1,84 @@
+/**
+ * 식자재 상세보기에 관한 javascript
+ */
+function goIngDetailUpDelForm(i_no){
+		var str = $("[name=ingredient_detail_form]").serialize();
+		location.replace("/onm/store_ingredient_detail_form.onm?"+str);
+	}
+
+
+function checkIngDeleteForm(){
+	if(confirm("정말 삭제하시겠습니까?")==false){
+		return;
+	}
+
+                           //else if(upDel=='up')
+//=============================================================================
+// 현재 화면에서 페이지 이동 없이 서버쪽 "${ctRoot}/boardUpDelProc.do" 를 호출하여
+// [게시판 수정 또는 삭제 적용 개수]를 받는다
+//=============================================================================
+	var i_no = $('[name=i_no]').val();
+	
+	 
+
+$.ajax({
+	// 접속할 서버쪽 URL 주소 설정
+	url : "store_ingredient_delete.onm"
+	// 전송 방법 설정
+	,type : "post"
+	// 서버로 보낼 파라미터명과 파라미터값을 설정
+	,data : "i_no="+i_no
+	
+	//서버의 응답을 성공적으로 받았을 경우 실행할 익명함수 설정
+	// 매개변수 upDelCnt 에는 [게시판 수정 또는 삭제 적용 개수]가 들어온다.
+	,success : function(delete_result){
+			if(delete_result==1){
+				alert("삭제 성공!");
+				location.replace("/onm/store_ingredient_form.onm")
+			}else if(delete_result==-1){
+				alert("게시물이 삭제되어 수정할 수 없습니다!");
+				location.replace("/onm/store_ingredient_form.onm")
+			}else if(delete_result==-2){
+				alert("비밀번호가 잘못 입력 되었습니다!");
+			}else if(delete_result==0){
+				
+				alert(dd);
+			}else{
+				alert("서버쪽 DB 연동 실패!");
+			}
+	}                    //,success : function(upDelCnt)
+
+	
+	//서버의 응답을 못 받았을 경우 실행할 익명함수 설정
+	,error : function(){
+		alert("서버와 통신 실패!");
+	}                           //,error : function()
+
+
+});                        //$.ajax({
+
+
+}                             // checkBoardUpDelForm(upDel)
+
+$(document).ready(function(){
+	
+
+	
+	//--------------------------------------------------------------------
+	// 페이징 처리 관련 HTML 소스를 class=pagingNumber 가진 태그 안에 삽입하기
+	//--------------------------------------------------------------------
+	$(".pagingNumber").html(
+		getPagingNumber(
+			"#"						// 검색 결과 총 행 개수
+			,"#"		// 선택된 현재 페이지 번호
+			,"#"		// 페이지 당 출력행의 개수
+			,"#"									// 페이지 당 보여줄 페이지번호 개수
+			,"goSearch();"							// 페이지 번호 클릭 후 실행할 자스코드
+		)
+	);
+
+});
+
+
+
+
